75508a1b2741fb71071311ba330a48a02ec903e9
danielle076 <d.vandenakker@novi-education.nl>
2021-02-15 10:37:44 +0100

    added files, installed files and checked files

---
+++ main.js
@@ -0,0 +1,9 @@
+// - [x] JS, HTML & CSS files maken
+// - [x] JavaScript script file linken
+// - [x] CSS script file linken
+// - [x] npm init (installeert package.json)
+// - [x] Axios installeren (npm install axios: hiermee kun je data ophalen, installeert package-lock.json)
+// - [x] Axios script linken (<script src="./node_modules/axios/dist/axios.min.js"></script>)
+// - [x] Check of files JS en CSS werken
+
+console.log("Hello there");
