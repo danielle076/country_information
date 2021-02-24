f3c75551015fc434b8624f1b09b4869c186e6953
danielle076 <d.vandenakker@novi-education.nl>
2021-02-24 08:38:43 +0100

    added button html + js

--- main.js
+++ main.js
@@ -6,4 +6,19 @@
 // - [x] Axios script linken (<script src="./node_modules/axios/dist/axios.min.js"></script>)
 // - [x] Check of files JS en CSS werken
 
-console.log("Hello there");
+// Vraag 1
+async function displayCountry(){
+    const country = "Belgie";
+    const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
+    console.log("GET COUNTRY HERE:", url, axios) // geeft link + axios
+
+    const response = await axios.get(url)
+    console.log("RESPONSE:", response); // geeft data Array met België
+}
+
+const button = document.getElementById("search-button");
+button.addEventListener("click", displayCountry);
+
+
+
+
